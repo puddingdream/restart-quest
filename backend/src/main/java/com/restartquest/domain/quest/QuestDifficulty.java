45d4792bb3e6c8351a232d7193a582ca72eae450
puddingdream <edcrfv098@naver.com>
2026-08-02 00:05:37 +0900

    [TASK-002] backend-2 TASK-002-DAG-428755fc98-02-be-quest-domain

---
+++ backend/src/main/java/com/restartquest/domain/quest/QuestDifficulty.java
@@ -0,0 +1,7 @@
+package com.restartquest.domain.quest;
+
+public enum QuestDifficulty {
+    EASY,
+    MEDIUM,
+    HARD
+}
---
+++ backend/src/main/java/com/restartquest/domain/quest/QuestStatus.java
@@ -0,0 +1,7 @@
+package com.restartquest.domain.quest;
+
+public enum QuestStatus {
+    TODO,
+    DONE,
+    REDESIGNED
+}
